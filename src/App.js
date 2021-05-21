3f5dca78a7155b1590623017444996bbea060ee4
Rahat Rahman <63892310+rahatbd@users.noreply.github.com>
2021-05-20 22:31:07 -0400

    added HowItWorks component and styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Nav from './Nav';
 import Header from './Header';
+import HowItWorks from './HowItWorks';
 
 const App = () => (
   <div className="wrapper">
     <Nav />
     <Header />
+    <HowItWorks />
   </div>
 );
 
