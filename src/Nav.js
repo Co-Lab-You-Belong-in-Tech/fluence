d55489451b9587085eed004987892aec3da71c45
Rahat Rahman <63892310+rahatbd@users.noreply.github.com>
2021-05-22 23:39:51 -0400

    updated nav styles

--- src/Nav.js
+++ src/Nav.js
@@ -1,25 +1,36 @@
-import { NavLink } from 'react-router-dom';
+import { useLocation, NavLink } from 'react-router-dom';
 import logo from './logo.svg';
 
-const Nav = () => (
-  <nav>
-    <ul className="wrapper">
-      <li>
-        <NavLink to="/">
-          <img src={logo} alt="logo" />
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          to="/calculator"
-          activeStyle={{ textDecoration: 'underline' }}
-          className="nav-calculator"
-        >
-          Calculator
-        </NavLink>
-      </li>
-    </ul>
-  </nav>
-);
+const Nav = () => {
+  // assigning location variable
+  const location = useLocation();
+  // destructuring pathname from location
+  const { pathname } = location;
+  // JavaScript split method to get the name of the path in array
+  const splitLocation = pathname.split('/');
+
+  return (
+    <nav>
+      <ul className="wrapper">
+        <li>
+          <NavLink to="/">
+            <img src={logo} alt="logo" />
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/calculator"
+            activeStyle={{ textDecoration: 'underline' }}
+            className={`nav-calculator ${
+              splitLocation[1] === 'analysis' ? 'nav-underline' : ''
+            }`}
+          >
+            Calculator
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default Nav;
